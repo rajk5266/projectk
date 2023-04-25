aedfa270f5ff22cbadc3081c14c9f75aa1edcec7
rajk5266 <kushwaharaj903@gmail.com>
2023-04-26 01:30:05 +0530

    selectedlistcolor

--- dom.js
+++ dom.js
@@ -1,15 +1,15 @@
 
-var x = document.getElementById('header-title')
-let main = document.getElementById('main-header')
-main.style.borderBottom = 'solid 6px blue'
-// x.style.borderBottom = 'solid 5px #000'
+
 
 var a = document.getElementsByClassName('list-group-item')
 
 for(let i=0; i<a.length; i++)
 {
-   a[i].style.backgroundColor = 'yellow'
+    if(i == 2)
+    {
+        a[i].style.backgroundColor = 'green'
+    }
    a[i].style.fontWeight = 'bold'
-   a[i].textContent = 'sold out'
+//    a[i].textContent = 'sold out'
 
 }
